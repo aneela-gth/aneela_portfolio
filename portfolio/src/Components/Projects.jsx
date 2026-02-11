c1781cc688413ffb28a90cbc0bbc448d5da37de3
aneela-gth <aneelabandi9951@gmail.com>
2026-02-11 13:38:10 +0530

    change

--- portfolio/src/Components/Projects.jsx
+++ portfolio/src/Components/Projects.jsx
@@ -19,7 +19,7 @@ const Projects = () => {
         {/* Project 2 */}
         <div className="project-card">
           <img src="/creative-studio-website-hero-section_328186-original.webp" alt="Salon Website" />
-          <h3>Beauty Salon Website</h3>
+          <h3>istudo </h3>
           <ul>
             <li>Responsive website using HTML & CSS</li>
             <li>Used Flexbox and Grid layouts</li>
@@ -37,6 +37,17 @@ const Projects = () => {
             <li>Secure REST API with CRUD operations</li>
             <li>Smooth & responsive UI</li>
             <li><strong>Tech:</strong> HTML, CSS, JavaScript,React</li>
+          </ul>
+        </div>
+         {/* Project 4 */}
+        <div className="project-card">
+          <img src="/1592348.jpg" alt="Notes App" />
+          <h3>imdb App</h3>
+          <ul>
+            <li>One of the world’s largest movie & TV databases</li>
+            <li>Information on movies, TV shows, and web series</li>
+            <li>User ratings & reviews for films and shows</li>
+            <li>Details about actors, directors, and celebrities</li>
           </ul>
         </div>
       </div>
